b23fd5f8027fa23029d698752185a69ac165ee86
Daniel Levy Moreno <daniellevymoreno@gmail.com>
2019-12-08 13:20:23 +0100

    Added lsp mode

--- python.el
+++ python.el
@@ -4,15 +4,37 @@
 
 ;;; Code:
 
-(use-package elpy
-  :defer t
-  :init
-  (advice-add 'python-mode :before 'elpy-enable)
+;; (use-package elpy
+;;   :defer t
+;;   :init
+;;   (advice-add 'python-mode :before 'elpy-enable)
+;;   :custom
+;;   (python-shell-interpreter "ipython")
+;;   (python-shell-interpreter-args "-i --simple-prompt")
+;;   :config
+;;   ;; remove flymake
+;;   (setq elpy-modules (delq 'elpy-module-flymake elpy-modules)))
+
+(use-package python-mode
+  :after ipython-shell-send
+  :ensure nil
+  :bind
+  ("C-c C-s" . ipython-shell-send-string)
+  ("C-c C-c" . ipython-shell-send-buffer)
+  ("C-c C-l" . ipython-shell-send-defun)
+  ("C-M-x"   . ipython-shell-send-region))
+
+(use-package ipython-shell-send)
+
+(use-package lsp-python-ms
   :custom
   (python-shell-interpreter "ipython")
   (python-shell-interpreter-args "-i --simple-prompt")
-  :config
-  (setq elpy-modules (delq 'elpy-module-flymake elpy-modules)))
+  :hook
+  (python-mode . (lambda ()
+                          (require 'lsp-python-ms)
+                          (lsp))))
+
 
 (provide 'python)
 ;;; python.el ends here
